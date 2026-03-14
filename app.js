35c0411a374361bc3b682d1a8ad72cbbf17eb434
Nikhil Kumar Gupta <148623766+guptaNikhil89@users.noreply.github.com>
2026-03-14 10:38:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let btn = document.querySelector('button');
-let heading = document.querySelector('h3');
+let heading = document.querySelector('h3')
 
 btn.addEventListener("click", function(){
     
     heading.innerText = generateColor();
-
     let div = document.querySelector('div');
     div.style.backgroundColor = heading.innerText;
 })
@@ -14,5 +13,6 @@ let generateColor = function(){
     let green = Math.floor(Math.random()* 255) ;
     let blue = Math.floor(Math.random()* 255) ;
     
-    return `rgb(${red}, ${green}, ${blue})`
+   return `rgb(${red}, ${green}, ${blue})`
+    
 }
